1690b43411c4475d9215801f583e12cad4dbba16
jiwon <jidole041214@naver.com>
2022-03-29 15:56:40 +0900

    add 전체 취업률 추가

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -9,6 +9,10 @@ export default function Header({ menuIndex }: Props) {
   return (
     <Wrapper>
       <Menu menuIndex={menuIndex} />
+      <div className="fullEmployment">
+        <h3>취업률 : 83%</h3>
+        <span>2020년 7월 17일 기준</span>
+      </div>
     </Wrapper>
   );
 }
@@ -17,4 +21,18 @@ const Wrapper = styled.header`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  & .fullEmployment {
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 5px;
+    & h3 {
+      font-size: 23px;
+    }
+    & span {
+      font-size: 15px;
+      color: var(--sub-text);
+    }
+  }
 `;
