b1f7ea0e93bc08758abfb6a9a39076287338d04c
jiwon <jidole041214@naver.com>
2022-03-29 14:40:54 +0900

    update 학과 카드 애니메이션

--- components/DepartmentDetail/index.tsx
+++ components/DepartmentDetail/index.tsx
@@ -1,18 +1,33 @@
 import styled from "@emotion/styled";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import DepartmentCard from "./Card";
 
 export default function DepartmentDetail() {
-  const arr = [
+  const [nowIndex, setNowIndex] = useState(0);
+  let arr = [
     "https://img4.yna.co.kr/photo/yna/YH/2017/09/22/PYH2017092216660001300_P4.jpg",
     "http://www.anewsa.com/news_images/2016/10/20/mark/20161020143111.jpg",
     "https://i.ytimg.com/vi/uTZaPdXB3aY/maxresdefault.jpg",
     "https://static.news.zumst.com/images/51/2017/09/23/b30697bd4ef44f62a59be38697b3cb12.jpg",
   ];
+  const [list, setList] = useState(arr);
+  useEffect(() => {
+    let index = 0;
+    setInterval(() => {
+      arr.push(arr[index % 4]);
+      setList(arr);
+      setNowIndex(index + 1);
+      index++;
+    }, 10000);
+  }, []);
   return (
     <Wrapper>
-      {arr.map((_, index) => (
-        <DepartmentCard index={index} src={_} key={index} />
+      {list.map((_, index) => (
+        <Fragment key={index}>
+          {index - nowIndex >= 0 && (
+            <DepartmentCard index={index - nowIndex} src={_} />
+          )}
+        </Fragment>
       ))}
     </Wrapper>
   );
